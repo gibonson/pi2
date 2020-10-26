4fce8bd070a54f6b741d4180227637538b9d729c
gibonson <gibonson@users.noreply.github.com>
2020-10-26 23:34:23 +0100

    nnnn

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+define('DATABASE_USER', 'user');
+define('DATABASE_HOST', 'localhost');
+define('DATABASE_PASSOWD', 'user');
+define('DATABASE_NAME', 'smartpi');
+define('DIR_TEMPLATES', '/home/pi/www/templates');
+define('DIR_IOTLIB', '/home/pi/www/iotLibraries');
+define('DIR_BOXES', '/home/pi/www/userFiles/jsonBoxes');
---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+include 'Router.php';
+include 'config.php';
+
+$url = $_SERVER['REQUEST_URI'];
+$router = new Router($url);
+
+$log = new \app\log\LogAction();
+
+//form
+$router->get("eventAction", "app/form/EventAction.php", $log);
+$router->get("readerAction", "app/form/ReaderAction.php", $log);
+
+//os function
+$router->get("777", "app/osOperation/Chmod777.php", $log);
+
+//app function
+$router->get("logAction", "app/log/LogAction.php");
+$router->get("eventController", "app/EventController.php");
+
+//content
+$router->get("index", "templates/content/MainContent.php", $log);
+$router->get("tables", "templates/content/Tables.php", $log);
+
+//404
+new \templates\MainPage("templates/MenuBar.php", "templates/404.php");
